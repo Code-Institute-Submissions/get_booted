4c1dae5bb25aac75b315057720aad47244f00e77
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-22 18:13:16 +0000

    Blog set up connecting no loading

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog/index.html'
+    template_name = 'blog/blog.html'
 
 class PostDetail(generic.DetailView):
     model = Post
-    template_name = 'blog/index.html'
+    template_name = 'blog/post_detail.html'
