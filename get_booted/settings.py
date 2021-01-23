f5db4123be0ec46b82c94a65b0d60527c2c9a274
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-23 16:41:38 +0000

    Updated region

--- get_booted/settings.py
+++ get_booted/settings.py
@@ -188,7 +188,7 @@ if 'USE_AWS' in os.environ:
 
 if 'USE_AWS' in os.environ:
     AWS_STORAGE_BUCKET_NAME = 'tomod24-get-booted'
-    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
