9658be4f15b71f0849501a2c402980a55226d7ad
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-24 21:30:41 +0000

    404 and 500 pages updated settings and folder move

--- home/templates/home/urls.py
+++ home/templates/home/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
+    path('404/', include('404.urls')),
+    path('500/', include('500.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='home')
 ]
-
 if settings.DEBUG:
     urlpatterns = [
-        path("404", http404),
-        path("500", http500),
+        path("404", views.http404),
+        path("500", views.http500),
     ] + urlpatterns
-
-    
