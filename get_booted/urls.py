8fd1c8ccc01cec46a0335c8e4621519b94dd54a7
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-24 21:44:38 +0000

    Folder destination updated due to error move

--- get_booted/urls.py
+++ get_booted/urls.py
@@ -16,7 +16,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -26,6 +25,4 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     path('blog/', include('blog.urls')),
-    path('404/', include('404.urls')),
-    path('500/', include('500.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
