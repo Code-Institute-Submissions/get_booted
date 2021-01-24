8b53162a44138e490cc1514ad215bf01d3fd77c0
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-24 21:02:40 +0000

    404 and 500 pages loaded

--- home/urls.py
+++ home/urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home')
 ]
+
+if settings.DEBUG:
+    urlpatterns = [
+        path("404", http404),
+        path("500", http500),
+    ] + urlpatterns
+
+    
