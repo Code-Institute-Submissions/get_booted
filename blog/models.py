ed86754686332117b6614341f1e47ac11e039cd8
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-27 02:54:37 +0000

    Admin and models udpated blog style amends and set up

--- blog/models.py
+++ blog/models.py
@@ -22,4 +22,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CharField)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' %(self.post.title, self.name)
+
 
