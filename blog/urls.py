2799d570f0f49a3d4ae8d8620ad18f15563da620
tomod24 <thomo_d24@hotmail.co.uk>
2021-01-28 01:31:54 +0000

    blog urls and post detail update to add comments

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/comment/', views.AddCommentView.as_view(), name='add_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
-from django.views import generic
-from .models import Post
-
-class PostList(generic.ListView):
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post, Comment
+from .forms import CommentForm
+class PostList(ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/blog.html'
-
-class PostDetail(generic.DetailView):
+class PostDetail(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+class AddCommentView(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'blog/add_comment.html'
+    success_url = reverse_lazy("blog")
